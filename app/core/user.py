266ddde4eda7f7cfe46847d3d13e2e6e5d707d61
Alexey <theivlev@yandex.ru>
2023-12-12 23:38:11 +0400

    project_cat_3

--- app/core/user.py
+++ app/core/user.py
@@ -19,41 +19,29 @@ from app.schemas.user import UserCreate
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
 
-
-# Определяем транспорт: передавать токен будем
-# через заголовок HTTP-запроса Authorization: Bearer.
-# Указываем URL эндпоинта для получения токена.
 bearer_transport = BearerTransport(tokenUrl='auth/jwt/login')
+lifetime_sec = 3600
+length_password = 3
 
 
-# Определяем стратегию: хранение токена в виде JWT.
 def get_jwt_strategy() -> JWTStrategy:
-    # В специальный класс из настроек приложения
-    # передаётся секретное слово, используемое для генерации токена.
-    # Вторым аргументом передаём срок действия токена в секундах.
-    return JWTStrategy(secret=settings.secret, lifetime_seconds=3600)
+    return JWTStrategy(secret=settings.secret, lifetime_seconds=lifetime_sec)
 
 
-# Создаём объект бэкенда аутентификации с выбранными параметрами.
 auth_backend = AuthenticationBackend(
-    name='jwt',  # Произвольное имя бэкенда (должно быть уникальным).
+    name='jwt',
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
 )
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-
-    # Здесь можно описать свои условия валидации пароля.
-    # При успешной валидации функция ничего не возвращает.
-    # При ошибке валидации будет вызван специальный класс ошибки
-    # InvalidPasswordException.
     async def validate_password(
         self,
         password: str,
         user: Union[UserCreate, User],
     ) -> None:
-        if len(password) < 3:
+        if len(password) < length_password:
             raise InvalidPasswordException(
                 reason='Password should be at least 3 characters'
             )
@@ -62,15 +50,12 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
                 reason='Password should not contain e-mail'
             )
 
-    # Пример метода для действий после успешной регистрации пользователя.
     async def on_after_register(
             self, user: User, request: Optional[Request] = None
     ):
-        # Вместо print здесь можно было бы настроить отправку письма.
         print(f'Пользователь {user.email} зарегистрирован.')
 
 
-# Корутина, возвращающая объект класса UserManager.
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
 
--- app/models/donation.py
+++ app/models/donation.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+
 from sqlalchemy import Boolean, Column, DateTime, Integer, Text, ForeignKey
+
 from app.core.db import Base
 
 
